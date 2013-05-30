3165753855df80e0a2ff586857f58bbaa76aab49
Mark McGuire <mark.b.mcg@gmail.com>
2013-05-30 17:05:43 +0000

    add packaging files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='blink1py',
+    url='https://github.com/TronPaul/blink1py',
+    version='0.1.0',
+    author='Mark McGuire',
+    author_email='mark.b.mcg@gmail.com',
+    packages=['blink1py'],
+    license='LICENSE.txt',
+    description='blink(1) library wrapper',
+    long_description=open('README.rst').read(),
+)
